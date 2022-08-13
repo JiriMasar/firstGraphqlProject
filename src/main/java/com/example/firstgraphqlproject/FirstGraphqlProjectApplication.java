a62b4ff8840ccb80e148bd3018b70830cf8e0a8d
Jiří Masař <jiri-masar@centrum.cz>
2022-08-13 07:34:12 +0200

    init

---
+++ src/main/java/com/example/firstgraphqlproject/FirstGraphqlProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstgraphqlproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstGraphqlProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstGraphqlProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstgraphqlproject/FirstGraphqlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstgraphqlproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstGraphqlProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
